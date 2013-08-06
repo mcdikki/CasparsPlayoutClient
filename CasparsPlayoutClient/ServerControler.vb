98063cd5fd9ccb8a473f2c126c03820faaa6ec99
Christopher Diekkamp <=>
2013-08-06 22:07:37 +0200

    Copyright and license header added to all src files

--- CasparsPlayoutClient/ServerControler.vb
+++ CasparsPlayoutClient/ServerControler.vb
@@ -1,4 +1,20 @@
-﻿Imports System.Threading
+﻿'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'' Author: Christopher Diekkamp
+'' Email: christopher@development.diekkamp.de
+'' GitHub: https://github.com/mcdikki
+'' 
+'' This software is licensed under the 
+'' GNU General Public License Version 3 (GPLv3).
+'' See http://www.gnu.org/licenses/gpl-3.0-standalone.html 
+'' for a copy of the license.
+''
+'' You are free to copy, use and modify this software.
+'' Please let know of any changes and improofments you made to it.
+''
+'' Thank you!
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+Imports System.Threading
 Imports CasparCGNETConnector
 Imports logger
 
