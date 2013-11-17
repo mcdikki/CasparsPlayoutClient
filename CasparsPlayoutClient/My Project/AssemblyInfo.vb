4c7135cb9b4126df321f93eeb41d4729d3f06549
Christopher Diekkamp <=>
2013-11-17 15:40:07 +0100

    Updated *Version number and added tag for release 0.1.1.5 beta*

--- CasparsPlayoutClient/My Project/AssemblyInfo.vb
+++ CasparsPlayoutClient/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.1.4")> 
+<Assembly: AssemblyVersion("0.1.1.5")> 
 <Assembly: AssemblyFileVersion("0.1.1.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
